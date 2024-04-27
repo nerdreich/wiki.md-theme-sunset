117b8d16cee5d781f134ea1107d1725396849384
nerdreich <git@nerdreich.at>
2024-04-27 14:05:44 +0200

    added remember-author feature across logouts

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# v2.3.0
+
+* added remember-author feature across logouts (requires js)
+
 # v2.2.0
 
 * moved mono font from Source Code Pro to Noto Sans Mono (variable 75%)
--- gulpfile.js
+++ gulpfile.js
@@ -18,12 +18,14 @@ import { readFileSync } from 'fs'
 import { deleteAsync } from 'del'
 
 import autoprefixer from 'gulp-autoprefixer'
+import browserify from 'browserify'
 import concat from 'gulp-concat'
 import gulp from 'gulp'
 import gzip from 'gulp-gzip'
 import replace from 'gulp-replace'
 import sort from 'gulp-sort'
 import tar from 'gulp-tar'
+import vinylSource from 'vinyl-source-stream'
 import zip from 'gulp-zip'
 
 import * as dartSass from 'sass'
@@ -44,6 +46,18 @@ gulp.task('clean', async () => {
     [dirs.build] + '/**/*',
     [dirs.build] + '/**/.*'
   ])
+})
+
+gulp.task('js', () => {
+  return browserify([
+    'src/js/main.js'
+  ])
+    .transform('babelify', {
+      presets: ['@babel/preset-env']
+    })
+    .bundle()
+    .pipe(vinylSource('main.js'))
+    .pipe(gulp.dest(dirs.theme))
 })
 
 gulp.task('fonts', () => {
@@ -92,7 +106,7 @@ gulp.task('favicon', () => {
     .pipe(gulp.dest(dirs.theme))
 })
 
-gulp.task('theme', gulp.parallel('fonts', 'scss', 'php', 'favicon', 'I18N'))
+gulp.task('theme', gulp.parallel('fonts', 'scss', 'php', 'js', 'favicon', 'I18N'))
 
 gulp.task('dist', gulp.series('clean', 'theme'))
 
